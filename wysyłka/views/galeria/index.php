bd448da7c088d23d4b52822c664aa41fbee611f7
Łukasz Malinowski <35465571+Streamline00950@users.noreply.github.com>
2018-02-01 21:06:38 +0100

    Add files via upload

---
+++ wysyłka/views/galeria/index.php
@@ -0,0 +1,58 @@
+<div class="sticky">
+       <a href="<?php echo ROOT_URL; ?>wesele"><img src="<?php echo ROOT_PATH; ?>assets/img/daltonicon1.png"  alt="daltonicon" title="Zmień kontrast" style="position: fixed;
+	top: 13px;
+	right: 0;
+	width: 35px;
+	z-index: 1;
+	background: white;
+	border: 1px solid black;">
+      </a>
+      </div>
+
+
+<div class="galeria-page">
+      <div class="row p-0 text-center" style="margin-top:200px;">
+                  <div id="heading" style="margin:0 auto;">
+                        <h2>Galeria</h2>
+                        <hr>
+                        <p>WYBRANE KOMPOZYCJE</p>
+                  </div>
+            </div>
+            <div class="row">
+                  <p class="text-center" style="width:50%; margin:auto;">
+                              Nasza firma wychodzi na przeciw najwyższym wymaganiom klientów. Wieloletnie doświadczenie sprawia, że znamy
+                              się na tym co robimy, a co ważniejsze kochamy to. Nasza firma jest firmą rodzinną dlatego tak jak
+                              nikt inny dbamy o to by klient był usatysfakcjonowany. Jeżeli wybór jest prosty to jest i zarazem
+                              satysfakcjonujący
+                        <!-- Pochwal się swoim bukietem, oceń bukiety innych użytkowników i zgarniaj nagrody! -->
+                  </p>
+            </div>
+
+      <div class="row text-center mt-4">
+            <div class="col-sm-12" style="margin-top:180px; margin-bottom:35px">
+                  <h3>Najczęściej wybierane przez klientów</h3>
+            </div>
+      </div>
+      <div class="container-fluid" id="gallerySlider">
+            <div id="SliderItem0"></div>
+            <div id="SliderItem1"></div>
+            <div id="SliderItem2"></div>
+            <div id="SliderItem3"></div>
+            <div id="SliderItem4"></div>
+      </div>
+      <div class="container" id="gallery">
+            <div class="row m-4">
+                  <h3>Pozostałe bukiety</h3>
+            </div>
+            <div class="row">
+                  <div class="col-sm-3 gImage"><img src="assets/img/flower (1).jpg" class="img-fluid" alt="">
+                  <p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH;?>assets/img/flower(2).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH; ?>assets/img/flower (3).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH;?>assets/img/flower (4).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH;?>assets/img/flower (5).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH;?>assets/img/flower (6).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH;?>assets/img/flower (1).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+                  <div class="col-sm-3 gImage"><img src="<?php echo ROOT_PATH;?>assets/img/flower(2).jpg" class="img-fluid" alt=""><p class="text-center"></p></div>
+            </div>
+      </div
